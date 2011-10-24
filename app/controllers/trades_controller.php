2502067e551e8a13546ec4a69fafe12338844a22
jtsvn <jtsvn@06155125-2a2b-4cf3-9046-6c9352ce0d4b>
2011-10-24 15:16:01 +0000

    git-svn-id: https://www.lpr.co/svn/ASAP@9 06155125-2a2b-4cf3-9046-6c9352ce0d4b

--- app/controllers/trades_controller.php
+++ app/controllers/trades_controller.php
@@ -2,25 +2,77 @@
 class TradesController extends AppController {
 	var $helpers = array ('Html','Form');
 	var $name = 'Trades';
+	var $funds = array();
+	
 	
 	function index() {
-		$this->set('trades', $this->Trade->find('all'));
-	}
+		$this->setchoices();
+		$conditions=array(
+			'Trade.crd >' => date('Y-m-d', strtotime($this->data['Trade']['daterange'])),
+			'Trade.fund_id =' => $this->data['Trade']['fundchosen'],
+			'Trade.broker_id =' => $this->data['Trade']['brokerchosen']
+		);
+		
 	
+	
+		$params=array(
+			'conditions' => $conditions, //array of conditions
+			//'recursive' => 1, //int
+			//'fields' => array('Model.field1', 'DISTINCT Model.field2'), //array of field names
+			'order' => array('Trade.crd DESC') //string or array defining order
+			//'group' => array('Model.field'), //fields to GROUP BY
+			//'limit' => n, //int
+			//'page' => n, //int
+			//'offset'=>n, //int   
+			//'callbacks' => true //other possible values are false, 'before', 'after'
+		);
+		
+		$this->set('trades', $this->Trade->find('all', $params));
+		
+		//echo "$this->data";
+		//echo "funds=";
+		//print_r($this->Trade->$funds);	// view variable not visible to controller
+	}
+		
 	function add() {
-		$this->set('funds', $this->Trade->Fund->find('list', array('fields'=>array('Fund.fund_name'))));
-		$this->set('secs', $this->Trade->Sec->find('list', array('fields'=>array('Sec.sec_name'))));
-		$this->set('tradeTypes', $this->Trade->TradeType->find('list', array('fields'=>array('TradeType.trade_type'))));
-		$this->set('reasons', $this->Trade->Reason->find('list', array('fields'=>array('Reason.reason_desc'))));
-		$this->set('brokers', $this->Trade->Broker->find('list', array('fields'=>array('Broker.broker_name'))));
-		$this->set('traders', $this->Trade->Trader->find('list', array('fields'=>array('Trader.trader_name'))));
+		$this->setchoices();
 	
 		if (!empty($this->data)) {
 			if ($this->Trade->saveAll($this->data)) {
 				$this->Session->setFlash('Your trade has been saved.');
 				$this->redirect(array('action' => 'index'));
 			}
-		} 
+		}
+	}
+	
+	
+	function edit($id = null) {
+		$this->setchoices();
+		$this->Trade->id = $id;
+		
+		if (empty($this->data)) {
+			$this->data = $this->Trade->read();
+		} else {
+			if ($this->Trade->save($this->data)) {
+				$this->Session->setFlash('Your trade has been updated.');
+				$this->redirect(array('action' => 'index'));
+			}
 	}
 }
+
+	
+	function setchoices() {
+		$this->set('funds', $this->Trade->Fund->find('list', array('fields'=>array('Fund.fund_name'))));
+		$this->set('secs', $this->Trade->Sec->find('list', array('fields'=>array('Sec.sec_name'))));
+		$this->set('tradeTypes', $this->Trade->TradeType->find('list', array('fields'=>array('TradeType.trade_type'))));
+		$this->set('reasons', $this->Trade->Reason->find('list', array('fields'=>array('Reason.reason_desc'))));
+		$this->set('brokers', $this->Trade->Broker->find('list', array('fields'=>array('Broker.broker_name'))));
+		$this->set('traders', $this->Trade->Trader->find('list', array('fields'=>array('Trader.trader_name'))));
+		
+		//if ($mode==1) {
+		//	array_unshift($brokers,"All","All");
+		//};
+	}
+}
+
 ?>
--- app/views/trades/index.ctp
+++ app/views/trades/index.ctp
@@ -1,11 +1,24 @@
 <!-- File: /app/views/trades/index.ctp -->
 
-<h1>Trades</h1>
+<h2>Trades</h2>
 
 <?php echo $this->Html->link('Add Trade', array('controller' => 'trades', 'action' => 'add')); ?>
 
 <table>
+<tr>
+<?php echo $this->Form->create();?>
+<td><?php echo $this->Form->input('daterange',array('type'=>'select','options'=>array('-1 week'=>'Last Week','-1 month'=>'Last Month','-1 year'=>'Last Year'),'label'=>'Input Date Range'));?></td>
+<td><?php echo $this->Form->input('fundchosen',array('type'=>'select','options'=>$funds,'label'=>'Choose Fund'));?></td>
+<td><?php echo $this->Form->input('brokerchosen',array('type'=>'select','options'=>$brokers,'label'=>'Choose Broker'));?></td>
+<td><?php echo $this->Form->end('Filter');?></td>
+</tr>
+</table>
+
+
+
+<table>
 	<tr>
+		<th>Edit</th>
 		<th>Id</th>
 		<th>Fund Id</th>
 		<th>Security Id</th>
@@ -25,13 +38,14 @@
 
 	<?php foreach ($trades as $trade): ?>
 	<tr>
+		<td><?php echo $this->Html->link('Edit', array('action' => 'edit', $trade['Trade']['id']));?></td>
 		<td><?php echo $trade['Trade']['id']; ?></td>
-		<td><?php echo $trade['Trade']['fund_id']; ?></td>
-		<td><?php echo $trade['Trade']['sec_id']; ?></td>
-		<td><?php echo $trade['Trade']['trade_type_id']; ?></td>
-		<td><?php echo $trade['Trade']['reason_id']; ?></td>
-		<td><?php echo $trade['Trade']['broker_id']; ?></td>
-		<td><?php echo $trade['Trade']['trader_id']; ?></td>
+		<td><?php echo $trade['Fund']['fund_name']; ?></td>
+		<td><?php echo $trade['Sec']['sec_name']; ?></td>
+		<td><?php echo $trade['TradeType']['trade_type']; ?></td>
+		<td><?php echo $trade['Reason']['reason_desc']; ?></td>
+		<td><?php echo $trade['Broker']['broker_name']; ?></td>
+		<td><?php echo $trade['Trader']['trader_name']; ?></td>
 		<td><?php echo $trade['Trade']['quantity']; ?></td>
 		<td><?php echo $trade['Trade']['broker_contact']; ?></td>
 		<td><?php echo $trade['Trade']['trade_date']; ?></td>
