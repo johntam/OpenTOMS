4912e4746c4cf499d5d8dace35e61a07520058b1
jtsvn <jtsvn@06155125-2a2b-4cf3-9046-6c9352ce0d4b>
2011-10-31 16:12:18 +0000

    git-svn-id: https://www.lpr.co/svn/ASAP@32 06155125-2a2b-4cf3-9046-6c9352ce0d4b

---
+++ app/controllers/prices_controller.php
@@ -0,0 +1,57 @@
+<?php
+class PricesController extends AppController {
+	var $helpers = array ('Html','Form');
+	var $name = 'Prices';
+
+	function index() {		
+		if (!empty($this->params['pass'])) {
+			$a = $this->params['pass'][0];
+		} else {
+			$a = 'A';
+		};
+		
+		$conditions=array(
+			'Sec.sec_name LIKE ' => $a.'%'
+		);
+	
+		$params=array(
+			'conditions' => $conditions, 
+			'fields' => array('Sec.id', 'Sec.sec_name', 'ticker', 'tradarid', 'currency', 'valpoint'),
+			'order' => array('Sec.sec_name ASC') 
+		);
+		
+		$this->set('secs', $this->Sec->find('all', $params));
+	}
+	
+	function add() {
+		$this->set('secTypes', $this->Sec->SecType->find('list', array('fields'=>array('SecType.sec_type_name'))));
+		
+		if (!empty($this->data)) {
+			if ($this->Sec->save($this->data)) {
+				$this->Session->setFlash('Security has been saved.');
+				$this->redirect(array('action' => 'view', $this->Sec->id));
+			}
+		}
+	}
+	
+	function edit($id = null) {
+		$this->Sec->id = $id;
+		$this->set('secTypes', $this->Sec->SecType->find('list', array('fields'=>array('SecType.sec_type_name'))));
+		
+		if (empty($this->data)) {
+			$this->data = $this->Sec->read();
+		} else {
+			if ($this->Sec->save($this->data)) {
+				$this->Session->setFlash('Security has been updated.');
+				$this->redirect(array('action' => 'view',$id));
+			}
+		}
+	}
+	
+	function view($id = null) {
+		$this->Sec->id = $id;
+		$this->set('sec', $this->Sec->read());
+	}
+
+}
+?>
---
+++ app/models/price.php
@@ -0,0 +1,8 @@
+<?php
+
+class Price extends AppModel {
+    var $name = 'Price';
+	var $belongsTo = 'Sec';
+}
+
+?>
