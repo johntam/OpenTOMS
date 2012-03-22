e27479d07eacdfba92dec9d05b2dc421b477190d
jtsvn <jtsvn@06155125-2a2b-4cf3-9046-6c9352ce0d4b>
2012-03-22 14:21:48 +0000

    git-svn-id: https://www.lpr.co/svn/ASAP@146 06155125-2a2b-4cf3-9046-6c9352ce0d4b

--- app/models/holding.php
+++ app/models/holding.php
@@ -11,12 +11,12 @@ class Holding extends AppModel {
 		$holdings = $this->find('all', array('conditions'=>array('Holding.fund_id =' => $fund,
 																 'Holding.trade_date <=' => $date,
 																 'Holding.cancelled =' => 0,
-																 'Holding.executed =' =>1),
+																 'Holding.executed =' => 1,
+																 'Holding.act =' => 1),
 											 'fields'=>array('Sec.sec_name', 'SUM(Holding.quantity) AS quantity'),
-											 'group'=>array('Sec.sec_name HAVING quantity <> 0'),
+											 'group'=>array('Sec.id', 'Sec.sec_name HAVING quantity <> 0'),
 											 'order'=>array('Sec.sec_type_id="2" ASC', 'Sec.sec_name ASC'),
-											 'contain' => array('Sec')));
-				
+											 'contain' => array('Sec')));		
 		return $holdings;
 	}
 }
